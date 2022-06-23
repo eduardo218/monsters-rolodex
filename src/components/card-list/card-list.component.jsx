2d12719114e6dd9bc45be9d9d7ef30ca258cea7c
Eduardo <eduardo.neves5@outlook.com>
2022-06-23 18:36:04 -0300

    Finalização do projeto

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,24 @@
+import { Component } from "react";
+
+import CardContainer from "../card-container/card-container.component";
+import './card-lis.styles.css'
+
+class CardList extends Component{
+    render(){
+        const {monsters} = this.props
+
+        return(
+            <div className="card-list">
+                {
+                    monsters.map(monster => {                        
+                        return(
+                            <CardContainer monster={monster}/>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default CardList;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+import './search-box.styless.css'
+
+class SearchBox extends Component{
+    
+    render(){
+        const {onChangeHandler, placeholder, className} = this.props;
+
+        return(
+            <input 
+                className={`search-box ${className}`}
+                type='search' 
+                placeholder={placeholder}
+                onChange={onChangeHandler}
+            />                    
+        )
+    }
+}
+
+export default SearchBox;
